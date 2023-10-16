748e49511c144a2e2c3e3a5399ff19ba53f2f0ad
Thomas-Ruby <thomasruby@ufl.edu>
2023-10-15 22:04:28 -0400

    Nav Bar updated

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 function Navbar(){  
 
     return (
-    <nav className="NavigationBar">
-        <h3>Navigation Bar</h3>     
-        <ul>         
-            <li><Link to="/">Games</Link></li>         
-        </ul>    
-    </nav>
+    <div>
+        <div className="NavigationIcon">
+            <i class="fa-solid fa-bars fa-2xl"></i>
+        </div>
+        <nav className="NavigationBar">
+            <h3>Navigation Bar</h3>     
+            <ul>         
+                <li><Link to="/">Games</Link></li>         
+            </ul>    
+        </nav>
+    </div>
     ); 
 } 
 export default Navbar; 
