c06b021954a2e2d7bb4659aba89de25e6aff3ab9
Thomas-Ruby <thomasruby@ufl.edu>
2023-10-22 11:56:24 -0400

    Navigation bar reformat and added CSS color vars

---
+++ src/pages/ColorSwitcher.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ColorSwitcher() {  
+
+    return (
+    <div className="ColorSwitcher">
+        <p><i class="fa-solid fa-palette fa-sm"></i>Color Switcher</p>
+    </div>
+    ); 
+};
+
+export default ColorSwitcher; 
